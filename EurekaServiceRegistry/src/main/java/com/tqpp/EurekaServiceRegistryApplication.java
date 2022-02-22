a000205ae5213fce8fbea64f8e34ee1c978c95d9
Rathod Avinash Ganesh <avinashrathod535@gmail.com>
2022-02-22 19:09:25 +0530

    MicroServices-2

---
+++ EurekaServiceRegistry/src/main/java/com/tqpp/EurekaServiceRegistryApplication.java
@@ -0,0 +1,15 @@
+package com.tqpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceRegistryApplication.class, args);
+	}
+
+}
